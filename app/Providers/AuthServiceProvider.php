f60232aada509310aa920ec0a438789509c560d9
ruhi16 <hndas2016@gmail.com>
2019-01-02 01:40:07 +0530

    AuthServ updated for Mysql

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Laravel\Passport\Passport;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Schema;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,7 +26,7 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-
+        Schema::defaultStringLength(191);
         Passport::routes();
     }
 }
